ab9dd3f798e76bba2a291fde66531698824a1d20
Daniel Holguin <39926374+MaxiNite@users.noreply.github.com>
2021-02-20 17:35:22 -0700

    Cambio de nombre a Alumno

---
+++ src/alumno/Alumno.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alumno;
+
+/**
+ *
+ * @author Dhtey
+ */
+public class Alumno {
+    private String nombre, apellido;
+    int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + '}';
+    }
+    
+}
